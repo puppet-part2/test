1d5132cf14c6546c279bc89d6d4a7a3ebdd6ee36
puppet-part2 <423495062@qq.com>
2022-04-21 17:46:29 +0800

    Update afl-llvm-rt.o.c

--- lto_mode/afl-llvm-rt.o.c
+++ lto_mode/afl-llvm-rt.o.c
@@ -345,6 +345,8 @@ void __sanitizer_cov_trace_pc_guard_init(uint32_t *start, uint32_t *stop)
     fclose(fpRead);  
   }
 
+  afl_map_size = 65960;
+
   if (!__afl_area_initial)
   {
     __afl_area_initial = (u8 *)malloc(afl_map_size * sizeof(u8));
